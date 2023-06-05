882537d2cdf7dcd80b6f8b8d19bd8b8e949e0df9
PEYZ <zhiani2000@gmail.com>
2023-06-05 15:32:47 -0700

    added new post a new user and get all current users

--- student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Controller/UserController.java
+++ student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.studenttrackercurd.studenttrackerbackend.Controller;
 
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,10 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepo.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepo.findAll();
     }
 }
--- student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Model/User.java
+++ student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Model/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="user")
+@Table(name="Students")
 public class User {
     
     @Id
