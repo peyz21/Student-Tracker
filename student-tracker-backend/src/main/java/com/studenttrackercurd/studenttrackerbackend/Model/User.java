1be402ed066300561615fb0dd39dfc7b483ffa59
PEYZ <zhiani2000@gmail.com>
2023-06-05 00:43:45 -0700

    added Controller,Exception,Model, and repo folder,  andd user under model

---
+++ student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Model/User.java
@@ -0,0 +1,72 @@
+package com.studenttrackercurd.studenttrackerbackend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String hairColor;
+    private Integer weight;
+    private Integer height;
+    private Float gpa;
+    private String email;
+    private String password;
+
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getHairColor() {
+        return hairColor;
+    }
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+    public Integer getWeight() {
+        return weight;
+    }
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+    public Integer getHeight() {
+        return height;
+    }
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+    public Float getGpa() {
+        return gpa;
+    }
+    public void setGpa(Float gpa) {
+        this.gpa = gpa;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
