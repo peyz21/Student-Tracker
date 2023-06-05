0814bb6beeb9b08a6835d13f4f4b57f925710185
PEYZ <zhiani2000@gmail.com>
2023-06-04 23:16:20 -0700

    initial commit

---
+++ student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/StudentTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studenttrackercurd.studenttrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ student-tracker-backend/src/test/java/com/studenttrackercurd/studenttrackerbackend/StudentTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studenttrackercurd.studenttrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
