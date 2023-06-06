dea6a0c18d4f517a635513759095021ee786274f
PEYZ <zhiani2000@gmail.com>
2023-06-05 18:21:37 -0700

    editted and added the navbar

---
+++ student-tracker-frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
