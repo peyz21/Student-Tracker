1ac110eb413aec6d612adf027063417827b7055d
PEYZ <zhiani2000@gmail.com>
2023-06-06 00:38:41 -0700

    added CrossOrigin which connects the back with the front

--- student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Controller/UserController.java
+++ student-tracker-backend/src/main/java/com/studenttrackercurd/studenttrackerbackend/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.studenttrackercurd.studenttrackerbackend.Controller;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,17 +12,18 @@ import com.studenttrackercurd.studenttrackerbackend.Model.User;
 import com.studenttrackercurd.studenttrackerbackend.Repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     
     @Autowired
     private UserRepository userRepo;
 
-    @PostMapping("/user")
+    @PostMapping("/student")
     User newUser(@RequestBody User newUser){
         return userRepo.save(newUser);
     }
 
-    @GetMapping("/users")
+    @GetMapping("/students")
     List<User> getAllUsers(){
         return userRepo.findAll();
     }
